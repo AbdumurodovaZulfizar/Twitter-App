51b3177b25106335ac5b577f4caf7b0c41f715ed
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-27 21:33:52 +0500

    Add user_id to tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_124255) do
+ActiveRecord::Schema.define(version: 2021_06_27_145513) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "tweet"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
