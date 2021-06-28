c90fdfc613bfe92de61ad51e58366b8fa9eaaccf
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-28 13:34:50 +0500

    Fix bugs

--- config/application.rb
+++ config/application.rb
@@ -5,13 +5,13 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 
 module Ajoyib
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.assets.initialize_on_precompile = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
